cc7ce382c008958a1c1425524d5685a1f0477197
Moroni Pereira <moronispereira@gmail.com>
2024-11-22 14:10:46 -0300

    Projeto concluido - commit final

--- src/main/java/com/mtech/workshopmongo/domain/User.java
+++ src/main/java/com/mtech/workshopmongo/domain/User.java
@@ -1,17 +1,27 @@
 package com.mtech.workshopmongo.domain;
 
 import java.io.Serializable;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document(collection="user")
 public class User  implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String name;
 	private String email;
 	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+	
 	public User() {
-		
 	}
 
 	public User(String id, String name, String email) {
@@ -45,9 +55,20 @@ public class User  implements Serializable{
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -59,9 +80,12 @@ public class User  implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
 
 }
---
+++ src/main/java/com/mtech/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.mtech.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mtech.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
